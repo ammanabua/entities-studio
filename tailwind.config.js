1768daad835cb53d3f3335c4c73a2a22f71550dc
Amman Abua <ammanabua@gmail.com>
2023-06-20 10:46:55 +0100

    Revert "update Schemas"
    This reverts commit bc556b23ab5cee028edc7420ceefd5cf65d7548f.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'krona': ['Krona One', 'sans-serif'],
-        'rubik': ['Rubik' , 'sans-serif'],
-        'barrio': ['Barrio', 'sans-serif']
-      }
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './components/**/*.{js,ts,jsx,tsx,mdx}',
+      './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          'krona': ['Krona One', 'sans-serif'],
+          'rubik': ['Rubik' , 'sans-serif'],
+          'barrio': ['Barrio', 'sans-serif']
+        }
+      },
     },
-  },
-  plugins: [
-    function ({ addVariant }) {
-      addVariant('child', '& > *');
-      addVariant('child-hover', '& > *:hover');
-    }
-  ],
-}
+    plugins: [
+      function ({ addVariant }) {
+        addVariant('child', '& > *');
+        addVariant('child-hover', '& > *:hover');
+      }
+    ],
+  }
+  
