a79c101cee61aa5b2ce199ea0f99ed1a7882a537
Amman Abua <ammanabua@gmail.com>
2023-06-20 09:26:34 +0100

    Revert "update Schemas" again
    This reverts commit bc556b23ab5cee028edc7420ceefd5cf65d7548f.

--- app/models/Art.js
+++ app/models/Art.js
@@ -12,11 +12,6 @@ const ArtSchema = new mongoose.Schema({
         required: true
     },
 
-    image: {
-        type: String,
-        required: true
-    },
-
     isOwned: {
         type: Boolean,
         default: false,
