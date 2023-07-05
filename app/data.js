8d7c5ca8c03b7d497f18c8f6561f2ee9cb14e3c0
Amman Abua <ammanabua@gmail.com>
2023-07-05 15:09:58 +0100

    update family portrait slider

--- app/data.js
+++ app/data.js
@@ -7,57 +7,57 @@ export const collections = [
       {
         name: "Welcome Home",
         intro: "Welcoming a future home with both old and new family members living, learning and experiencing life together. Embedded with a new set of values that tie our bonds closer together.",
-        url: "family-portrait/welcome-home.webp"
+        url: "/family-portrait/welcome-home.webp"
       },
       {
         name: "Not Forgotten People",
         intro: "To my extended links that I never took the time to form an authentic bond with. This piece is an active reminder to continue to make an effort and reach out to my community at large.",
-        url: "family-portrait/not-forgotten.webp"
+        url: "/family-portrait/not-forgotten.webp"
       },
       {
         name: "The Inner Circle",
         intro: "Within my family bonds; some relationships are strong, some are mild and others are still forming. If we call ourselves a family, it’s our duty to take the time to assist each connection to thrive.",
-        url: "family-portrait/inner-circle.webp"
+        url: "/family-portrait/inner-circle.webp"
       },
       {
         name: "Ties",
         intro: "The connections flowing between my immediate family which extends and grows as we bond with others.",
-        url: "family-portrait/ties.webp"
+        url: "/family-portrait/ties.webp"
       },
       {
         name: "Grandmas Shadow",
         intro: "In honor of my Grandmothers. May their memories within me continue to connect me to a vibrant and industrial heritage of strength, love and vitality.",
-        url: "family-portrait/grandmas-shadow.webp"
+        url: "/family-portrait/grandmas-shadow.webp"
       },
       {
         name: "Blue Soldier Riddle",
         intro: "In the face of adversities, he learns from your mistakes and studying his opponents. He remains open to every outcome as he knows he shall conquer and gain more knowledge exploring his adventurous spirit",
-        url: "family-portrait/blue-soldier.webp"
+        url: "/family-portrait/blue-soldier.webp"
       },
       {
         name: "Voids",
         intro: "Patience comes at a price, when you live on dreams and hopes. Bide your time, wait your turn - a pit of wants are formed as you ignore the world around you.",
-        url: "family-portrait/voids.webp"
+        url: "/family-portrait/voids.webp"
       },
       {
         name: "First Date",
         intro: "Happy Anniversary Dad and Mom. A relationship that started a new extension of our family lineage. Their relationship taught me lots of values that will be a foundation to my own extension. Be playful and loving to each other.",
-        url: "family-portrait/first-date.webp"
+        url: "/family-portrait/first-date.webp"
       },
       {
         name: "Siblings",
         intro: "As siblings, you fight, you quarrel, you yell, you annoy each other but you laugh, you protect, you learn to enjoy each other’s company. For my sibling - My Besties",
-        url: "family-portrait/siblings.webp"
+        url: "/family-portrait/siblings.webp"
       },
       {
         name: "Ancestral Lines",
         intro: "An eternal connection I will always have with my culture, tradition and heritage, as nothing can sever the blood ties linking me to my ancestry. I carry them with me always.",
-        url: "family-portrait/ancestral-lines.webp"
+        url: "/family-portrait/ancestral-lines.webp"
       },
       {
         name: "My Family",
         intro: "A family portrait reflecting the different personalities within my family unit",
-        url: "family-portrait/my-family.webp"
+        url: "/family-portrait/my-family.webp"
       },
     ]
   },
--- components/FamilyPortraitSlider.jsx
+++ components/FamilyPortraitSlider.jsx
@@ -1,39 +1,59 @@
+'use client'
+import { useState } from "react"
 import Link from "next/link"
 import { motion } from "framer-motion"
 import Image from "next/image"
 
 
-const FamilyPortraitSlider = () => {
+const FamilyPortraitSlider = ({ images }) => {
+    
+    const [currentIndex, setCurrentIndex] = useState(0);
+    
+    const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex + 1 === images.length ? 0 : prevIndex + 1
+    );
+    };
+    const handlePrevious = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1);
+    };
+    const handleDotClick = (index) => {
+    setCurrentIndex(index);
+     }
+
+
   return (
     <div className='bg-[#EBE6D3] py-28 px-12 justify-center align-center items-center w-full'>
-        <div className='md:flex'>
+        <div className='sm:flex'>
             <div className='w-full md:w-1/2'>
-                <h3 className='text-xl md:text-4xl md:font-bold text-left'>Voids</h3>
-                <p className='font-rubik text-left text-xl font-light tracking-wide mb-12 w-full'>Patience comes at a price, when you live on dreams and hopes. Bide your time, wait your turn - a pit of wants are formed as you ignore the world around you.</p>
+                <h3 className='text-xl md:text-4xl md:font-bold text-left'>{images[currentIndex].name}</h3>
+                <p className='font-rubik text-left text-xl font-light tracking-wide mb-12 w-full'>{images[currentIndex].intro}</p>
             </div>
             
             <motion.div  
                 className='w-full md:w-1/2'>
                 <Link href="/collections/family-portrait/details">
                     <Image
-                        src='/collection1.png'
+                        key={currentIndex}
+                        src={images[currentIndex].url}
                         alt="Voids"
                         width="0"
                         height="0"
                         sizes="100vw"
                         className="w-full h-auto" />
                 </Link>
+                <div className='flex w-28 align-center justify-between mt-12 font-rubik'>
+                    <div className='w-12 h-12 bg-white items-center flex align-center justify-center font-light hover:bg-[#EB6F07] hover:text-white' onClick={handlePrevious}>
+                        ‹
+                    </div>
+                    <div className='w-12 h-12 bg-white items-center flex align-center justify-center font-light hover:bg-[#EB6F07] hover:text-white' onClick={handleNext}>
+                        ›
+                    </div>
+                </div>
             </motion.div>
         </div>
 
-        <div className='flex w-28 align-center justify-between mt-12 font-rubik'>
-            <Link href='/' className='w-12 h-12 bg-white items-center flex align-center justify-center font-light hover:bg-[#EB6F07] hover:text-white'>
-                ‹
-            </Link>
-            <Link href='/' className='w-12 h-12 bg-white items-center flex align-center justify-center font-light hover:bg-[#EB6F07] hover:text-white'>
-                ›
-            </Link>
-        </div>
     </div>
   )
 }
