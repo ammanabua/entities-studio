893405cdb745abf4e57d65b950de3fd7f1bf934f
Amman Abua <ammanabua@gmail.com>
2023-07-19 23:35:01 +0100

    update slider

--- components/FamilyPortraitSlider.jsx
+++ components/FamilyPortraitSlider.jsx
@@ -24,23 +24,23 @@ const FamilyPortraitSlider = ({ images }) => {
 
 
   return (
-    <div className='bg-[#EBE6D3] lg:bg-gradient-to-r lg:bg-gradient-90 from-black from-30% via-[#EBE6D3] via-30% to-[#EBE6D3] py-28 lg:py-8 px-12 min-h-screen align-center w-full lg:w-10/12 lg:m-auto overflow-hidden'>
+    <div className='bg-[#EBE6D3] lg:bg-gradient-to-r lg:bg-gradient-90 from-black from-25% via-[#EBE6D3] via-25% to-[#EBE6D3] py-28 lg:py-0 px-12 min-h-screen align-center w-full lg:w-10/12 lg:m-auto overflow-hidden'>
         
-        <div className='sm:flex justify-center align-center h-full items-center z-10 lg:absolute'>
+        <div className='sm:flex justify-center align-center h-full z-10 lg:absolute items-center'>
             
-            <div className='w-full md:w-1/2 px-8 lg:w-1/3 lg:order-2'>
+            <div className='w-full md:w-1/2 px-8 lg:w-1/3 lg:order-2 ml-8'>
                 <h3 className='text-xl md:text-4xl md:font-bold text-left tracking-wide'>{images[currentIndex].name}</h3>
                 <p className='font-rubik text-left text-xl font-light tracking-wide mb-12 w-full tracking-wide'>{images[currentIndex].intro}</p>
             </div>
             
             <motion.div  
-                className='w-full md:w-1/2 lg:w-1/3'>
+                className='w-full md:w-1/2 lg:w-1/3 mr-8'>
                 <Link href="/collections/family-portrait/details">
                     <Image
                         key={currentIndex}
                         src={images[currentIndex].url}
                         alt="Voids"
-                        width="0"
+                        width={490}
                         height="0"
                         sizes="100vw"
                         className="w-full lg:h-96" />
@@ -56,63 +56,63 @@ const FamilyPortraitSlider = ({ images }) => {
                         ›
                     </div>
                 </div>
-                <div className='invisible lg:visible flex w-full ml-72 align-center justify-between mt-28 bottom-0 font-rubik'>
-                    <div className="flex w-28 justify-between">
-                        <div className='w-12 h-12 bg-transparent items-center flex align-center justify-center font-light' onClick={handlePrevious}>
-                            ‹ Prev
-                        </div>
-                        <div className='w-12 h-12 bg-transparent items-center flex align-center justify-center font-light' onClick={handleNext}>
-                            Next ›
-                        </div>
+            </motion.div>
+            <div className='invisible lg:visible flex w-2/3 ml-32 items-center justify-between bottom-4 font-rubik z-40 text-[#777D96] absolute px-4'>
+                <div className="flex w-44 justify-between font-rubik text-xl font-medium">
+                    <div className='w-24 h-24 bg-transparent items-center flex align-center justify-center cursor-pointer' onClick={handlePrevious}>
+                        ‹ Prev
                     </div>
-                    <div>
-                        Page No
+                    <div className='w-24 h-24 bg-transparent items-center flex align-center justify-center cursor-pointer' onClick={handleNext}>
+                        Next ›
                     </div>
                 </div>
-            </motion.div>
+                <div className="font-rubik">
+                    <span className="text-4xl   text-[#32374B]">{currentIndex + 1}</span><span className="text-xl"> / {images.length}</span>
+                </div>
+            </div>
         </div>
 
         {/* LEFT SIDE SOCIAL LINKS */}
 
-        <div className="hidden lg:flex h-screen justify-center items-center align-center absolute left-36 top-60 mt-12 w-1/5 z-30 m-auto">
+        <div className="hidden lg:flex h-screen justify-center items-center align-center absolute left-24 w-1/5 z-30 m-auto">
             <div className="space-y-4">
                 <div className="bg-[#1A1A1A] rounded-full p-2">
-                    <Link href='https://www.tiktok.com' className="">
+                    <Link href='https://www.tiktok.com/@entities.studio' target="_blank" className="">
                         <Image
                             src='/tiktok.svg'
-                            height={20}
-                            width={20}
+                            height={15}
+                            width={15}
                             alt="Tiktok"
                             className="" />
                     </Link>
                     
                 </div>
                 <div className="bg-[#1A1A1A] rounded-full p-2">
-                    <Link href='https://www.youtube.com' className="">
+                    <Link href='https://www.youtube.com/@chimdidaentity' target="_blank" className="">
                         <Image
                             src='/youtube.svg'
-                            height={20}
-                            width={20}
+                            height={15}
+                            width={15}
                             alt="Twitter" />
                     </Link>
                     
                 </div>
                 <div className="bg-[#1A1A1A] rounded-full p-2">
-                    <Link href='https://www.instagram.com' className="">
+                    <Link href='https://www.instagram.com/entities_universe' target="_blank" className="">
                         <Image
                             src='/ig.svg'
-                            height={20}
-                            width={20}
+                            height={15}
+                            width={15}
                             alt="Twitter" />
                     </Link>
                     
                 </div>
                 <div className="bg-[#1A1A1A] rounded-full p-2">
-                    <Link href='https://www.twitter.com' className="">
+                    <Link href='https://www.twitter.com/chimdidaentity' target="_blank" className="">
                         <Image
                             src='/twitter.svg'
-                            height={20}
-                            width={20}
+                            height={15}
+                            width={15}
                             alt="Twitter"
                             className="" />
                     </Link>
