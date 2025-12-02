4eeb41d22862f372a44742c34e2eb1f9124ca765
Amman Abua <ammanabua@gmail.com>
2025-12-02 16:52:24 -0600

    add firebase app deployment

---
+++ src/services/firebase/config.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: "entities-studio",
+  storageBucket: "entities-studio.firebasestorage.app",
+  messagingSenderId: "368978493399",
+  appId: "1:368978493399:web:7e188fc31f9c8078c39592",
+  measurementId: "G-Z06YKWLQGQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
