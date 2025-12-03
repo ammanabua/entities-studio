1f2fe1ad37c8ba703a6b38cfb422cf5cb4997f26
Amman Abua <ammanabua@gmail.com>
2025-12-02 18:06:07 -0600

    fix deployment error

--- src/services/firebase/config.ts
+++ src/services/firebase/config.ts
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain: "entities-studio.firebaseapp.com",
   projectId: "entities-studio",
   storageBucket: "entities-studio.firebasestorage.app",
   messagingSenderId: "368978493399",
